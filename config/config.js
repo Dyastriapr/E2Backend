e93ad7c66fa987d995a860d129a2518d957f45bc
Dyastriapr <dyastriap1@gmail.com>
2024-07-05 13:13:01 +0700

    db

--- config/config.js
+++ config/config.js
@@ -1,33 +1,26 @@
-require("dotenv").config();
-
 module.exports = {
   development: {
-    username: process.env.EXPRESS_DB_USERNAME,
-    password: process.env.EXPRESS_DB_PASSWORD,
-    database: process.env.EXPRESS_DB_NAME,
-    host: process.env.EXPRESS_DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: "mysql",
-    port: process.env.EXPRESS_DB_PORT,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   },
-  test: {},
   production: {
-    username: process.env.EXPRESS_DB_USERNAME,
-    password: process.env.EXPRESS_DB_PASSWORD,
-    database: process.env.EXPRESS_DB_NAME,
-    host: process.env.EXPRESS_DB_HOST,
+    use_env_variable:
+      "mysql://ue7nauuuoyefckob:CtmFNaM6DPA0AcGZ6zFD@bfwyooz9tp4pksihicej-mysql.services.clever-cloud.com:3306/bfwyooz9tp4pksihicej",
     dialect: "mysql",
-    port: process.env.EXPRESS_DB_PORT,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
   },
 };
